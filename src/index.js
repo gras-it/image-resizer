b203177d2c0f7469fef97723885dfd14e6dc3e39
unknown <aylabbs@gmail.com>
2021-05-10 15:48:15 -0400

    add logo and frame

--- src/index.js
+++ src/index.js
@@ -1,53 +1,89 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
+const sharp = require('sharp')
+
 const port = process.env.PORT || 3010
 const Busboy = require('busboy');
-const resizer = require('./resize')
+const resize = require('./resize')
+// const resizer = require('./resize')
 const corsOptions = {
   origin: ['https://admin.graslawn.com', 'https://aspire-front-end.ngrok.io'],
   methods: ['GET', 'POST', 'OPTIONS'],
 }
-app.get('/', cors({origin: true}), (req, res) => {
-  res.send('I am up!')
-})
-app.post('/', cors({origin: true}), (req, res) => {
+const resizer = ({
+  rotate,
+  left,
+  top,
+  width,
+  height,
+  maxWidth,
+  logo
+}) => {
+  return sharp()
+    .rotate(rotate)
+    .extract({
+      left,
+      top,
+      width,
+      height,
+    })
+    .resize({ width: maxWidth })
+    .composite([{
+      input: logo,
+      top: ((maxWidth / width) * height) - 38,
+      left: 17
+    }])
+    .extend({
+      top: 7,
+      bottom: 7,
+      left: 7,
+      right: 7,
+      background: { r: 0, g: 52, b: 77, alpha: 1 }
+    })
+    .jpeg({
+      mozjpeg: true,
+      quality: 80
+    })
+    .on('error', e => console.log(e))
+}
 
-  const busboy = new Busboy({ headers: req.headers });
-  const {
-    rotate,
-    left,
-    top,
-    width,
-    height,
-    maxWidth
-  } = req.query
-  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-    console.log('File [' + fieldname + ']: filename: ' + filename);
 
-    file.on('end', function() {
-      console.log('File [' + fieldname + '] Finished');
+;(async () => {
+  const logo = await sharp('logo.png').resize({
+    width: 50,
+  }).toBuffer()
+  app.get('/', cors(corsOptions), (req, res) => {
+    res.send('I am up!')
+  })
+  app.post('/', cors(corsOptions), (req, res) => {
+    const busboy = new Busboy({ headers: req.headers });
+    const {
+      rotate,
+      left,
+      top,
+      width,
+      height,
+      maxWidth
+    } = req.query
+    busboy.on('file', file => {
+      file.on('data', () => null)
+      file.pipe(resizer({
+        rotate: Number(rotate),
+        left: Number(left),
+        top: Number(top),
+        width: Number(width),
+        height: Number(height),
+        maxWidth: Number(maxWidth),
+        logo
+      })).pipe(res)
     });
-    file.pipe(resizer({
-      rotate: Number(rotate),
-      left: Number(left),
-      top: Number(top),
-      width: Number(width),
-      height: Number(height),
-      maxWidth: Number(maxWidth),
-    })).pipe(res)
-  });
-  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-    // console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-  });
-  busboy.on('finish', function() {
-    // console.log('Done parsing form!');
-    // res.writeHead(303, { Connection: 'close', Location: '/' });
-    // res.end();
-  });
-  req.pipe(busboy);
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    busboy.on('field', () => null);
+    busboy.on('finish', () => null));
+    req.pipe(busboy);
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+})()
